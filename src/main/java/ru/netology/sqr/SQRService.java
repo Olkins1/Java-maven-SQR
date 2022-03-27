e0bd87a8e814ce88badad974599a9415ed0e750e
Olga Novikova <novikova.os@gmail.com>
2022-03-27 16:06:42 +0300

    first commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,13 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int numberOfHits(int diapasonStart, int diapasonEnd) {
+        int hitCounter = 0;
+        for (int i=10; i <= 99; i++) {
+            int result = i * i;
+            if (result >= diapasonStart && result <=diapasonEnd) hitCounter++;
+        }
+
+        return hitCounter;
+    }
+}
