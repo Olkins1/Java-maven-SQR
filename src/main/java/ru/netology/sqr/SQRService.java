1bf785587ad55b8b4c59beae80361987416ecf5c
Olga Novikova <novikova.os@gmail.com>
2022-03-28 10:52:33 +0300

    Corrections made

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,9 +3,11 @@ package ru.netology.sqr;
 public class SQRService {
     public int numberOfHits(int diapasonStart, int diapasonEnd) {
         int hitCounter = 0;
-        for (int i=10; i <= 99; i++) {
+        for (int i = 10; i <= 99; i++) {
             int result = i * i;
-            if (result >= diapasonStart && result <=diapasonEnd) hitCounter++;
+            if (result >= diapasonStart && result <= diapasonEnd) {
+                hitCounter++;
+            }
         }
 
         return hitCounter;
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -2,22 +2,22 @@ package ru.netology.sqr;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
 
     @ParameterizedTest
     @CsvSource(
-            value={
+            value = {
                     "'zero hits',1,99,0",
                     "'one hit',101,122,1",
                     "'all hit ',100,9801,90",
             },
-            delimiter=','
+            delimiter = ','
     )
     void numberOfHits(String test, int diapStart, int diapEnd, int hitsExpected) {
-        SQRService service;
-        service = new SQRService();
+        SQRService service = new SQRService();
         int actual = service.numberOfHits(diapStart, diapEnd);
         assertEquals(hitsExpected, actual);
     }
